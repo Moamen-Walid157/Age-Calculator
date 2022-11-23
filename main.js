c50708dac24466cdecf86697fbb825551f460d71
Moamen Walid <116441371+Moamen-Walid157@users.noreply.github.com>
2022-11-23 17:51:44 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+var year =  document.getElementById("age");
+
+function age(){
+    document.getElementById("result").innerHTML=(" It Equales : "+ year.value*12 + " months .")
+    document.getElementById("result2").innerHTML=(" It Equales : "+ year.value*365 + " days .")
+
+}
